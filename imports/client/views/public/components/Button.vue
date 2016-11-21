caf869c632d505e9fb10db19e096da6c6002b5b1
Nizul Zaim <skynightz93@gmail.com>
2016-11-21 21:04:31 +0800

    :art: Delete comment in Button Doc

--- imports/client/views/public/components/Button.vue
+++ imports/client/views/public/components/Button.vue
@@ -1,16 +1,5 @@
 <template>
     <div>
-        <!-- <div  style="z-index:3;" class="background-theme" v-depth="1" id="index-header">
-            <page-container style="padding-top: 100px;">
-                <div class="row">
-                    <div class="col-md-fluid-12">
-                        <div class="font-display2 color-white" style="margin-top: 20px;">
-                            Button UI
-                        </div>
-                    </div>
-                </div>
-            </page-container>
-        </div> -->
         <transition-block :show="show">
             <div style="display:table; margin: 0 auto;">
                 <raised-button type="link" link="/" class="primary" v-ripple><icon name="menu"></icon> Link Button</raised-button>
