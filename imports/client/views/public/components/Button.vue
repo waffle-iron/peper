44537bcbc0c00f05d56805b2829c4a7d2083d502
Nizul Zaim <skynightz93@gmail.com>
2016-11-24 22:57:54 +0800

    :memo: Add Documentation for RaisedButton, FlatButton & ColorButton

--- imports/client/views/public/components/Button.vue
+++ imports/client/views/public/components/Button.vue
@@ -1,10 +1,154 @@
 <template>
     <div>
         <transition-block :show="show">
-            <div style="display:table; margin: 0 auto;">
-                <raised-button type="link" link="/" class="primary" v-ripple><icon name="menu"></icon> Link Button</raised-button>
-                <raised-button type="button" class="primary" v-ripple><icon name="menu"></icon> Link Button</raised-button>
-            </div>
+            <peper-doc>
+                <div slot="render" class="row center-xs">
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <raised-button type="button" v-ripple>Button</raised-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <raised-button type="button">Button</raised-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <raised-button type="button" class="primary" v-ripple>Button</raised-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <raised-button type="button" class="accent" v-ripple>Button</raised-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <raised-button type="button" disabled>Button</raised-button>
+                        </div>
+                    </div>
+                </div>
+                <span slot="label" class="row center-xs hidden-xs">
+                    <div class="col-sm-3">
+                        Default Raised
+                    </div>
+                    <div class="col-sm-3">
+                        Without Ripple
+                    </div>
+                    <div class="col-sm-3">
+                        Primary Raised
+                    </div>
+                    <div class="col-sm-3">
+                        Accent Raised
+                    </div>
+                    <div class="col-sm-3">
+                        Disabled
+                    </div>
+                </span>
+                <span slot="code" v-for="item in code1">
+                    <peper-code>{{item}}</peper-code>
+                </span>
+            </peper-doc>
+            <peper-doc>
+                <div slot="render" class="row center-xs">
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <flat-button type="button" v-ripple>Button</flat-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <flat-button type="button">Button</flat-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <flat-button type="button" class="primary" v-ripple>Button</flat-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <flat-button type="button" class="accent" v-ripple>Button</flat-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <flat-button type="button" disabled>Button</flat-button>
+                        </div>
+                    </div>
+                </div>
+                <span slot="label" class="row center-xs hidden-xs">
+                    <div class="col-sm-3">
+                        Default Flat
+                    </div>
+                    <div class="col-sm-3">
+                        Without Ripple
+                    </div>
+                    <div class="col-sm-3">
+                        Primary Flat
+                    </div>
+                    <div class="col-sm-3">
+                        Accent Flat
+                    </div>
+                    <div class="col-sm-3">
+                        Disabled
+                    </div>
+                </span>
+                <span slot="code" v-for="item in code2">
+                    <peper-code>{{item}}</peper-code>
+                </span>
+            </peper-doc>
+
+            <peper-doc>
+                <div slot="render" class="row center-xs">
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <color-button type="button" v-ripple>Button</color-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <color-button type="button">Button</color-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <color-button type="button" class="primary" v-ripple>Button</color-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <color-button type="button" class="accent" v-ripple>Button</color-button>
+                        </div>
+                    </div>
+                    <div class="col-sm-3 render-block">
+                        <div class="button-center-container">
+                            <color-button type="button" disabled>Button</color-button>
+                        </div>
+                    </div>
+                </div>
+                <span slot="label" class="row center-xs hidden-xs">
+                    <div class="col-sm-3">
+                        Default Color
+                    </div>
+                    <div class="col-sm-3">
+                        Without Ripple
+                    </div>
+                    <div class="col-sm-3">
+                        Primary Color
+                    </div>
+                    <div class="col-sm-3">
+                        Accent Color
+                    </div>
+                    <div class="col-sm-3">
+                        Disabled
+                    </div>
+                </span>
+                <span slot="code" v-for="item in code3">
+                    <peper-code>{{item}}</peper-code>
+                </span>
+            </peper-doc>
         </transition-block>
     </div>
 </template>
@@ -14,6 +158,27 @@
         data() {
             return {
                 show: false,
+                code1: [
+                    '<raised-button type="button" v-ripple>Button</raised-button>',
+                    '<raised-button type="button">Button</raised-button>',
+                    '<raised-button type="button" class="primary" v-ripple>Button</raised-button>',
+                    '<raised-button type="button" class="accent" v-ripple>Button</raised-button>',
+                    '<raised-button type="button" disabled>Button</raised-button>'
+                ],
+                code2: [
+                    '<flat-button type="button" v-ripple>Button</flat-button>',
+                    '<flat-button type="button">Button</flat-button>',
+                    '<flat-button type="button" class="primary" v-ripple>Button</flat-button>',
+                    '<flat-button type="button" class="accent" v-ripple>Button</flat-button>',
+                    '<flat-button type="button" disabled>Button</flat-button>'
+                ],
+                code3: [
+                    '<color-button type="button" v-ripple>Button</color-button>',
+                    '<color-button type="button">Button</color-button>',
+                    '<color-button type="button" class="primary" v-ripple>Button</color-button>',
+                    '<color-button type="button" class="accent" v-ripple>Button</color-button>',
+                    '<color-button type="button" disabled>Button</color-button>'
+                ]
             }
         },
         mounted() {
