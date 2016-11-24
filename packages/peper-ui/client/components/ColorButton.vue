ffa2a7e7f7ac2f431c22f69f7740ff13a450b4e6
Nizul Zaim <skynightz93@gmail.com>
2016-11-24 22:55:42 +0800

    :sparkles: Add ColorButton

---
+++ packages/peper-ui/client/components/ColorButton.vue
@@ -0,0 +1,50 @@
+<template>
+    <router-link :to="link" :tag="tag" class="color-button" :type="type">
+        <slot></slot>
+    </router-link>
+</template>
+
+<script>
+export default {
+    props: {
+        type: {
+            default: "link",
+            type: String,
+        },
+        link: {
+            default: "",
+            type: String,
+        }
+    },
+    computed: {
+        tag() {
+            if (this.type === "link") {
+                return "a"
+            }
+            return "button";
+        }
+    },
+    mounted() {
+        let self = this;
+        self.$nextTick(function() {
+            $el = $(self.$el);
+
+            $el.hover(function() {
+                $(this).addClass("hover");
+            }, function() {
+                $(this).removeClass("hover");
+            });
+
+            $el.on("touchstart click", function() {
+                var $this =$(this);
+                $this.trigger("mouseleave");
+                $this.blur();
+                $this.addClass("hover");
+                $this.one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(e) {
+                    $this.removeClass("hover");
+                });
+            })
+        })
+    }
+}
+</script>
