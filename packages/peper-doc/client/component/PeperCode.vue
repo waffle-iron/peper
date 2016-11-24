2b04a0a38977fd5d67e3adf732ef1ec89d77a73c
Nizul Zaim <skynightz93@gmail.com>
2016-11-24 22:56:36 +0800

    :lipstick: Add v-ripple to peper-code Copy button

--- packages/peper-doc/client/component/PeperCode.vue
+++ packages/peper-doc/client/component/PeperCode.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="peper-code">
-        <div class="copy-button">
+        <div class="copy-button" v-ripple>
             COPY
         </div>
         <pre><code class="html" style="max-width: 1170px;margin: 0 auto; padding-left:12px; padding-right: 54px;"><slot></slot></code></pre>
