8dbddcb99b52290f125ffc0658be0fb27007a517
Nizul Zaim <skynightz93@gmail.com>
2016-12-04 16:40:18 +0800

    :bug: Fix DropDown Menu not show exact location in display: flex

--- packages/peper-ui/client/components/DropdownMenu.vue
+++ packages/peper-ui/client/components/DropdownMenu.vue
@@ -7,7 +7,7 @@
 <script>
 export default {
     props: {
-        trigger: null,
+        trigger: {default: null},
         openOn: {default: 'click'},
         left: {
             type: Boolean,
@@ -39,47 +39,59 @@ export default {
         }
 
         let menuPosition = function() {
-            if (self.trigger !== undefined) {
+            if (self.trigger) {
                 let $trigger = $(self.trigger);
-                let $posLeft = $trigger.offset().left;
-                let $posTop = $trigger.offset().top;
+                let $posLeft;
+                let $posTop;
 
+                var CheckPos= function(cb) {
+                    let offset = $trigger.offset()
+                    $posLeft = offset.left;
+                    $posTop = offset.top;
+
+                    if (!self.top) {
+                        $posTop = $posTop + gap;
+                    } else {
+                        $posTop = $posTop + $trigger.outerHeight() - gap - $thisEl.outerHeight();
+                    }
+
+                    if (!self.left) {
+                        $posLeft = $posLeft + $trigger.outerWidth() - gap - $thisEl.outerWidth();
+                    } else {
+                        $posLeft = $posLeft + gap;
+                    }
+
+                    $thisEl.css({
+                        top: $posTop,
+                        left: $posLeft,
+                    })
+                    window.setTimeout(function() {
+                        cb();
+                    }, 100);
+                }
+
+                CheckPos(function() {});
                 if (!self.top) {
-                    $posTop = $posTop + gap;
                     $thisEl.addClass('bottom');
                 } else {
-                    $posTop = $posTop + $trigger.outerHeight() - gap - $thisEl.outerHeight();
                     $thisEl.addClass('top');
                 }
 
                 if (!self.left) {
-                    $posLeft = $posLeft + $trigger.outerWidth() - gap - $thisEl.outerWidth();
                     $thisEl.addClass('right');
                 } else {
-                    $posLeft = $posLeft + gap;
                     $thisEl.addClass('left');
                 }
 
-                $thisEl.css({
-                    top: $posTop,
-                    left: $posLeft,
-                })
-
                 let $children = $thisEl.children();
                 let childrenLength = $children.length;
-                // let count = 1;
-                // $children.each(function() {
-                //     $(this).addClass('fade-top-right');
-                //     $(this).css('transition-delay', (count * 0.05 + 0.2) + "s");
-                //     count++;
-                // })
-                $trigger.unbind( self.openOn + '.menu');
                 $trigger.bind(self.openOn  + '.menu', function() {
-                    $thisEl.toggleClass('show');
+                    CheckPos(function() {
+                        $thisEl.toggleClass('show');
+                    });
                 });
 
                 var enableClick = false;
-                $(document).unbind('mouseup.menu');
                 $(document).on("mouseup.menu", function(e) {
                     if ($thisEl.hasClass('show') && enableClick) {
                         inActive();
@@ -93,16 +105,18 @@ export default {
                     enableClick = false;
                 }
 
-                $("*").unbind('scroll.menu');
                 $("*").bind("scroll.menu", function() {
                     inActive();
                 })
             }
         }
-        menuPosition();
 
-        $(window).on("resize",function() {
+        self.$nextTick(function() {
             menuPosition();
+
+            $(window).on("resize",function() {
+                menuPosition();
+            })
         })
     }
 };
